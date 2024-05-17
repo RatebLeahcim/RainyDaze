bb748456e6f3e7852fef3300ad076ce32e07525a
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-05-16 23:06:13 -0400

    PlayerMovement Script
    [SerializeField] private Rigidbody2D m_rigidbody;
    [SerializeField] private Vector2 m_velocity;
    [SerializeField] private float m_acceleration;
    
    public Vector2 Velocity { get { return m_velocity; } }
    public float Aceleration { get { return m_acceleration; } }

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D m_rigidbody;
+    [SerializeField] private Vector2 m_velocity;
+    [SerializeField] private float m_acceleration;
+
     void Start()
     {
-        
+        m_rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HandleMovement()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            m_velocity.x += m_acceleration;
+        }
     }
 }
