bacb7e4745432b2cf3f3391117d6c946e6f0c87e
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-05-31 22:17:46 -0400

    Slope update
    small update to Ame so they can walk on slopes

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,15 +6,17 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    //--------------------------------------------------------------------------------
+    //Serialized Fields
+    //--------------------------------------------------------------------------------
+
     //Rigidbody2D and GroundCheck components
     [SerializeField] private Rigidbody2D m_rigidbody;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
     //Physics Variables
-    [SerializeField, Range(0f, 1f)] private float m_friction;
     [SerializeField] private float m_gravity = 9.81f;
-    [SerializeField] private float m_groundedGravity = 0.5f;
     [SerializeField] private float m_gravityModifer = 1.2f;
 
     //Walk Variables
@@ -28,24 +30,43 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float m_maxJumpTime = 0.5f;
     [SerializeField] private bool m_isJumpedPressed;
     [SerializeField] private bool m_canJump = true;
-
-    //Check Variables
-    [SerializeField] private bool m_isGrounded;
     [SerializeField] private bool m_isJumping;
-    private bool m_isFacingRight = true;
+    [SerializeField] private bool m_isGrounded;
+
+    //Slope Variables
+    [SerializeField] private float m_slopeCheckDistance;
+    [SerializeField] private float m_maxSlopeAngle = 45f;
+    [SerializeField] private bool m_isOnSlope;
+    [SerializeField] private bool m_canWalkOnSlope;
+
+    //Contact Filter
+    [SerializeField] private ContactFilter2D m_contactFilter;
+
+    //--------------------------------------------------------------------------------
+    //Private Variables
+    //--------------------------------------------------------------------------------
 
     private PlayerInput m_playerInput;
     private movementMode m_movementMode;
 
-    [SerializeField] private ContactFilter2D m_contactFilter;
-    [SerializeField] private Door door;
+    private bool m_isFacingRight = true;
+    private float m_slopeDownAngle;
+    private float m_slopeSideAngle;
+    private float m_slopeDownAngleOld;
+    private Vector2 m_slopeNormalPerp;
 
+    private float m_originalGravity;
+
+    //--------------------------------------------------------------------------------
     //Properties
+    //--------------------------------------------------------------------------------
+
     public Vector2 Velocity { get { return m_velocity; } set { m_velocity = value; } }
     public float Speed { get { return m_speed; } }
-    public float Friction { get { return m_friction; } set { m_friction = value; } }
     public float JumpSpeed { get { return m_jumpSpeed; } set { m_jumpSpeed = value; } }
     public bool IsGrounded { get {  return m_isGrounded; } }
+
+    //--------------------------------------------------------------------------------
 
     private void Awake()
     {
@@ -57,14 +78,16 @@ public class PlayerMovement : MonoBehaviour
         m_playerInput.PlayerControl.Movement.canceled += onMovementCancel;
         m_playerInput.PlayerControl.Jump.performed += OnJump;
         m_playerInput.PlayerControl.Jump.canceled += onJumpCancel;
+        m_playerInput.PlayerControl.Interact.started += OnInteract;
+
+        m_originalGravity = m_gravity;
 
         setUpJumpVariables();
-
-        m_playerInput.PlayerControl.Interact.started += OnInteract;
     }
 
     void FixedUpdate()
     {
+        CheckSlope();
         HandleJump();
         HandleMovement();
         HandleGravity();
@@ -80,8 +103,9 @@ public class PlayerMovement : MonoBehaviour
         m_playerInput.PlayerControl.Movement.canceled += onMovementCancel;
         m_playerInput.PlayerControl.Jump.performed += OnJump;
         m_playerInput.PlayerControl.Jump.canceled += onJumpCancel;
-
         m_playerInput.PlayerControl.Interact.started += OnInteract;
+
+        setUpJumpVariables();
     }
     void OnDisable()
     {
@@ -92,9 +116,6 @@ public class PlayerMovement : MonoBehaviour
         m_playerInput.PlayerControl.Movement.canceled -= onMovementCancel;
         m_playerInput.PlayerControl.Jump.performed -= OnJump;
         m_playerInput.PlayerControl.Jump.canceled -= onJumpCancel;
-
-        setUpJumpVariables();
-
         m_playerInput.PlayerControl.Interact.started -= OnInteract;
     }
     #endregion
@@ -123,6 +144,7 @@ public class PlayerMovement : MonoBehaviour
         m_isJumpedPressed = false;
     }
     #endregion
+
     #region Interact
     void OnInteract(InputAction.CallbackContext context)
     {
@@ -132,22 +154,17 @@ public class PlayerMovement : MonoBehaviour
         Collider2D[] interacbles = new Collider2D[1];
         int num = Physics2D.OverlapCollider(GetComponent<Collider2D>(), m_contactFilter, interacbles);
 
-        if (num > 0)
+        if(num <= 0)
         {
-            IInteractable interactable = interacbles[0].GetComponent<IInteractable>();
-            if (interactable != null)
-            {
-                Debug.Log("Interactable Found");
-                interactable.OnInteract();
-            }
-            else
-            {
-                Debug.Log("No Interactable Found");
-            }
+            Debug.Log("No Interactable Found");
+            return;
         }
-        else
+
+        IInteractable interactable = interacbles[0].GetComponent<IInteractable>();
+        if (interactable != null)
         {
-            Debug.Log("No Collisions Found");
+            Debug.Log("Interactable Found");
+            interactable.OnInteract();
         }
 
         //foreach (Collider2D collider in interacbles)
@@ -155,13 +172,11 @@ public class PlayerMovement : MonoBehaviour
         //    Debug.Log(collider.name);
         //}
     }
-
     #endregion
 
     #region Handlers
     private void HandleMovement()
     {
-        m_rigidbody.velocity = new Vector2(m_velocity.x * m_speed, m_velocity.y);
         m_isGrounded = CheckGrounded();
 
         //Handles player face direction
@@ -173,7 +188,20 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
+
+        //Handles player movement
+        if (m_isOnSlope && m_canWalkOnSlope)
+        {
+            m_rigidbody.velocity = new Vector2(m_velocity.x * m_speed * m_slopeNormalPerp.x, m_velocity.x * m_speed * m_slopeNormalPerp.y);
+            m_rigidbody.gravityScale = 0;
+        }
+        else
+        {
+            m_rigidbody.velocity = new Vector2(m_velocity.x * m_speed, m_velocity.y);
+            m_rigidbody.gravityScale = m_originalGravity;
+        }
     }
+
     private void HandleJump()
     {
         if(!m_isJumping && m_isGrounded && m_isJumpedPressed && m_canJump)
@@ -209,7 +237,65 @@ public class PlayerMovement : MonoBehaviour
     #region Helpers
     private bool CheckGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, .5f, groundLayer);
+    }
+
+    private void CheckSlope()
+    {
+        CheckSlopeHorizontal(groundCheck.position);
+        CheckSlopeVertcial(groundCheck.position);
+    }
+
+    private void CheckSlopeVertcial(Vector2 position)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, m_slopeCheckDistance, groundLayer);
+
+        if(hit)
+        {
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+            Debug.DrawRay(hit.point, m_slopeNormalPerp, Color.red);
+
+            m_slopeNormalPerp = - Vector2.Perpendicular(hit.normal).normalized;
+            m_slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if(m_slopeDownAngle != m_slopeDownAngleOld)
+            {
+                m_isOnSlope = true;
+            }
+
+            m_slopeDownAngleOld = m_slopeDownAngle;
+        }
+
+        if(m_slopeDownAngle > m_maxSlopeAngle)
+        {
+            m_canWalkOnSlope = false;
+        }
+        else
+        {
+            m_canWalkOnSlope = true;
+        }
+    }
+
+    private void CheckSlopeHorizontal(Vector2 position)
+    {
+        RaycastHit2D front = Physics2D.Raycast(position, transform.right, m_slopeCheckDistance, groundLayer);
+        RaycastHit2D back = Physics2D.Raycast(position, -transform.right, m_slopeCheckDistance, groundLayer);
+
+        if(front)
+        {
+            m_isOnSlope = true;
+            m_slopeSideAngle = Vector2.Angle(front.normal, Vector2.up);
+        }
+        else if (back)
+        {
+            m_isOnSlope = true;
+            m_slopeSideAngle = Vector2.Angle(back.normal, Vector2.up);
+        }
+        else
+        {
+            m_isOnSlope = false;
+            m_slopeSideAngle = 0.0f;
+        }
     }
     
     private void Flip()
